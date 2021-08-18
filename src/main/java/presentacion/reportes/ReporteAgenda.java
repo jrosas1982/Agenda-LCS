38c16493f31eb635b98c91577bba74147310d3b6
Juan Pablo <jrosas1982@gmail.com>
2021-08-17 23:29:55 -0300

    resta solucionar la libreria de reportes

--- src/main/java/presentacion/reportes/ReporteAgenda.java
+++ src/main/java/presentacion/reportes/ReporteAgenda.java
@@ -31,7 +31,9 @@ public class ReporteAgenda
 		Map<String, Object> parametersMap = new HashMap<String, Object>();
 		parametersMap.put("Fecha", new SimpleDateFormat("dd/MM/yyyy").format(new Date()));		
     	try		{
-			this.reporte = (JasperReport) JRLoader.loadObjectFromFile( "reportes" + File.separator + "ReporteAgenda.jasper" );
+			this.reporte = (JasperReport) JRLoader.loadObject( "reportes" + File.separator + "ReporteAgenda.jasper" );
+
+//			this.reporte = (JasperReport) JRLoader.loadObjectFromFile( "reportes" + File.separator + "ReporteAgenda.jasper" );
 			this.reporteLleno = JasperFillManager.fillReport(this.reporte, parametersMap, 
 					new JRBeanCollectionDataSource(personas));
     		log.info("Se cargó correctamente el reporte");
